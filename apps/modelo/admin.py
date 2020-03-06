2a0e44d8b9bb0314d7fe6300c968e6ecf1d46355
andersonquizhpe <jhander59@gmail.com>
2020-03-06 11:56:30 -0500

    modelos

--- apps/modelo/admin.py
+++ apps/modelo/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 # Register your models here.
 
 
-=======
-from .models import Autor, Editor, Libro
-# Register your models here.
-
-admin.site.register(Editor)
-admin.site.register(Autor)
-admin.site.register(Libro)
->>>>>>> 882eaf939e366be5a55531563af902ecfc76cced
